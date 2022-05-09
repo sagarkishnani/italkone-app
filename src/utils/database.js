3122c90e67d0dc57eada6b10b64a07bbe6fee61e
Sagar <sagarkishnani67@gmail.com>
2022-05-08 22:44:02 -0500

    Version 1.0

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,4 +1,4 @@
-export const URL_API = 'https://italkone-project-default-rtdb.firebaseio.com/';
-const API_KEY = 'AIzaSyBV3FVHsIcLKRaBoxBdDyUn165LLv4N39I';
+export const URL_API = '';
+const API_KEY = '';
 export const URL_AUTH_SIGNUP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 export const URL_AUTH_LOGIN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${API_KEY}`;
